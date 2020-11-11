43fc75f7a30cbb2e5a29aac178407ec7680dcb1e
Improviser14 <pavance40@gmail.com>
2020-11-10 22:38:23 -0500

    trying a Stack Overflow solution

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,59 @@
-var express  = require("express");
-var router   = express.Router();
+var express = require("express");
+var router = express.Router();
 var passport = require("passport");
-var User     = require("../models/user");
+var User = require("../models/user");
 
 // root route
-router.get('/', function(req, res){
-   res.render("landing"); 
+router.get("/", function (req, res) {
+  res.render("landing");
 });
 
-
 // show register form
-router.get("/register", function(req, res) {
-   res.render("register"); 
+router.get("/register", function (req, res) {
+  res.render("register");
 });
 
 // handle register/sign up logic
-router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
-    if(req.body.adminCode === process.env.ADMIN_CODE){
-        newUser.isAdmin = true;
-        console.log("adminCode: " + "encrypted and entered correctly");
+router.post("/register", function (req, res) {
+  var newUser = new User({ username: req.body.username });
+  if (req.body.adminCode === process.env.ADMIN_CODE) {
+    newUser.isAdmin = true;
+    console.log("adminCode: " + "encrypted and entered correctly");
+  }
+  User.register(newUser, req.body.password, function (err, user) {
+    if (err) {
+      req.flash("error", err.message);
+      return res.redirect("/register");
     }
-    User.register(newUser, req.body.password, function(err, user){
-       if(err){
-             req.flash("error", err.message);
-             return res.redirect("/register");
-       } 
-       passport.authenticate("local")(req, res, function(){
-          req.flash("success", "Welcome to roamHangout " + user.username); 
-          res.redirect("/topics"); 
-       });
-    }); 
+    passport.authenticate("local")(req, res, function () {
+      req.flash("success", "Welcome to roamHangout " + user.username);
+      res.redirect("/topics");
+    });
+  });
 });
 
 // show login form
-router.get("/login", function(req, res){
-   res.render("login"); 
+router.get("/login", function (req, res) {
+  res.render("login");
 });
-
 
 // handle login logic
-router.post("/login", passport.authenticate("local", 
-    {
-        successRedirect:"/topics",
-        failureRedirect:"/login",
-        failureFlash: true,
-        successFlash: "welcome back" 
-    }), function(req, res){
-});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/topics",
+    failureRedirect: "/login",
+    failureFlash: true,
+    successFlash: "welcome back",
+  }),
+  function (req, res) {}
+);
 
 // logout route
-router.get("/logout", function(req,res){
-   req.logout();
-   req.flash("success","Logged You Out");
-   res.redirect("/topics");
+router.get("/logout", function (req, res) {
+  req.logout();
+  req.flash("success", "Logged You Out");
+  res.redirect("/topics");
 });
 
 module.exports = router;
